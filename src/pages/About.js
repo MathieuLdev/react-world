a7cb91788a44cb7037eeb78bbab09481f47dd683
Mathieu <mathieu.linder.dev@gmail.com>
2021-10-05 19:53:39 +0200

    Complete project

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo'
+
+const About = () => {
+   return (
+      <div>
+         <Navigation />
+         <Logo />
+         <h1>À propos</h1>
+         <br />
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis aperiam excepturi beatae, officia, natus maiores nisi illo harum commodi quaerat ea! Odit nulla saepe, odio corrupti doloribus reiciendis laborum? Nemo numquam recusandae rem quam dolorem dolorum laborum. Consectetur sunt qui nostrum repellat voluptate modi architecto. Temporibus quo sunt soluta, vitae porro provident perspiciatis magnam est repudiandae. Atque neque quis dignissimos?</p>
+         <br />
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, culpa quas ducimus quibusdam quod deserunt. Quaerat sed explicabo necessitatibus tenetur ratione quae, eum quisquam, nam repellendus quo dolores harum nihil! Provident aliquid quasi natus voluptate vero, eligendi soluta, libero asperiores sed quis ex. Porro libero nobis, laborum quos officia corrupti tempore, nesciunt asperiores, aliquid eius tenetur? Porro itaque omnis neque iure qui sit recusandae maxime inventore cum fugiat nesciunt exercitationem sint expedita nisi, molestias obcaecati non quis consequatur dolore quasi, distinctio voluptate. Sapiente incidunt ab, nihil optio quibusdam est! Culpa iusto cupiditate quo voluptas aliquam quia commodi, soluta architecto necessitatibus nulla laboriosam fugiat, atque ex molestias consequatur eos error beatae doloremque. Veritatis unde possimus tenetur? Modi asperiores rerum voluptatem sapiente perferendis eius deleniti explicabo quibusdam? Dolor ipsa animi architecto accusamus, eligendi, rem quae maxime obcaecati minima sequi voluptate cupiditate consequuntur.</p>
+      </div>
+   );
+};
+
+export default About;
